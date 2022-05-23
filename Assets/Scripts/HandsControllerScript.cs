fbc4b108ccee80374e95ede36bdb507249595890
mona17n <mona17n@gmail.com>
2022-05-24 00:18:43 +0530

    Hand animation with script completed

--- Assets/Scripts/HandsControllerScript.cs
+++ Assets/Scripts/HandsControllerScript.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class HandsControllerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] InputActionReference gripInputAction;
+    [SerializeField] InputActionReference triggerInputAction;
+    Animator handAnimator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+private void Awake()
+{
+    handAnimator = GetComponent<Animator>();
 }
+
+private void OnEnable()
+{
+    gripInputAction.action.performed += GripPressed;
+    triggerInputAction.action.performed += TriggerPressed;
+}
+
+ private void TriggerPressed(InputAction.CallbackContext obj)
+ {
+     handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+
+ }
+
+ private void GripPressed(InputAction.CallbackContext obj)
+ {
+     handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+
+ }
+
+private void OnDisable()
+{ 
+    gripInputAction.action.performed -= GripPressed;
+    triggerInputAction.action.performed -= TriggerPressed;
+ 
+}
+
+
+
+}
+
